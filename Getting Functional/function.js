afa508cce0c8ea2b9994c3555c3474ab27a6447c
Abasola17 <aabasola@filamer.edu.ph>
2025-03-20 02:18:57 -0700

    add getting functional

---
+++ Getting Functional/function.js
@@ -0,0 +1,43 @@
+// var dogName = "Pao";
+// var dogWeight = 23;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says WOOF WOOF");
+// }
+
+// var dogName = "Pao";
+// var dogWeight = 23;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says WOOF WOOF");
+// }
+
+// var dogName = "Pao";
+// var dogWeight = 23;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says WOOF WOOF");
+// }
+
+// var dogName = "Pao";
+// var dogWeight = 23;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says WOOF WOOF");
+// }
+
+function bark (name, weight) {
+    if (weight > 20) {
+        console.log(name + "says WOOF WOOF");
+    }
+    else {
+        console.log(name + "sya WOOF WOOF");
+    }
+}
+
+bark("pao", 25);
+bark("mochi", 27);
