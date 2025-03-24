5f8a759a63473774619037038fff0159aac67fec
abasola17 <aabasola@filamer.edu.ph>
2025-03-24 01:39:29 -0700

    function activity 1

---
+++ Activities/Fuction - Activity/script.js
@@ -0,0 +1,14 @@
+function whatshallIwear(temp) {
+    if (temp < 60 ){
+        console.log("wear a jacket");
+    }
+    else if (temp < 70) {
+        console.log("wear t-shirt");
+    } else {
+        console.log("wear a sweater");
+    }
+}
+
+whatshallIwearhatshallIwear(50);
+whatshallIwearhatshallIwear(60);
+whatshallIwearhatshallIWear(80);
