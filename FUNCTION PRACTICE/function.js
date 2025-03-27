3832edbe32157eed03d4629706111a93a3e349b2
abasola17 <aabasola@filamer.edu.ph>
2025-03-27 02:18:35 -0700

    Function Practice

---
+++ FUNCTION PRACTICE/function.js
@@ -0,0 +1,23 @@
+// function calculateArea(r) {
+//     var area;
+//     if (r <= 0) {
+//         return 0;
+//     } else {
+//         area = Math.PI * r * r;
+//         return area;
+//     }
+// }
+
+// var radius = 5.2;
+// var theArea = calculateArea(radius);
+// console.log("The area is: " + theArea);
+
+function printName(firstName, lastName) {
+    var fullName = firstName + ' ' + lastName
+    return fullName;
+}
+
+var first = "Analyn joy"
+var last = "Abasola"
+
+console.log('My fullname is ', printName(first, last))
